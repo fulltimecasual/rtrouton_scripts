7e82dc80e0adedd3dce2477538720f995230506a
Peter Wells <fulltimecasual@gmail.com>
2019-05-27 16:08:43 +1000

    my-config
    Signed-off-by: Peter Wells <fulltimecasual@gmail.com>

--- rtrouton_scripts/Casper_Scripts/Jamf_Pro_Computer_Script_Download/Jamf_Pro_Computer_Script_Download.sh
+++ rtrouton_scripts/Casper_Scripts/Jamf_Pro_Computer_Script_Download/Jamf_Pro_Computer_Script_Download.sh
@@ -19,7 +19,7 @@
 # please enter the complete directory path into the ScriptDownloadDirectory
 # variable below.
 
-ScriptDownloadDirectory=""
+ScriptDownloadDirectory="/Users/pwells/Desktop/Scripts"
 
 # If the ScriptDownloadDirectory isn't specified above, a directory will be
 # created and the complete directory path displayed by the script.
